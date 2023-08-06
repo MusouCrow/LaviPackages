e618826bc71ce49a2c270059063e377d7a4df671
MusouCrow <939549924@qq.com>
2023-08-07 02:50:51 +0800

    添加颜色Lerp函数

--- com.koiyun.render-pipelines.lavi/ShaderLibrary/Functions.hlsl
+++ com.koiyun.render-pipelines.lavi/ShaderLibrary/Functions.hlsl
@@ -6,3 +6,9 @@ void ShadowAttenuation_float(float3 PositionWS, out float Attenuation)
 {
     Attenuation = ShadowAttenuation(PositionWS);
 }
+
+void LerpColor_float(float4 BaseColor, float4 LerpColor, out float4 Color)
+{
+    Color.rgb = lerp(BaseColor.rgb, LerpColor.rgb, LerpColor.a);
+    Color.a = BaseColor.a;
+}
