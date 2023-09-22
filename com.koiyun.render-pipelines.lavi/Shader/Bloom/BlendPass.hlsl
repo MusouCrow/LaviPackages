e46bfacca9093abc2d6290c3b10ae152241f0a56
MusouCrow <939549924@qq.com>
2023-09-23 00:28:52 +0800

    修改Bloom方案

--- com.koiyun.render-pipelines.lavi/Shader/Bloom/BlendPass.hlsl
+++ com.koiyun.render-pipelines.lavi/Shader/Bloom/BlendPass.hlsl
@@ -6,9 +6,10 @@ float4 Frag(Varyings input) : SV_Target
 {
     float4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_LinearClamp, input.uv);
     float4 blur = SAMPLE_TEXTURE2D(_BloomBlurTexture, sampler_LinearClamp, input.uv);
+    half brightness = Max3(color.r, color.g, color.b) * 0.5;
 
     color.rgb += blur.rgb;
-    color.rgb = lerp(color.rgb, blur.rgb, saturate(blur.a));
+    color.rgb = lerp(color.rgb, blur.rgb, saturate(blur.a * (1 - brightness)));
     
     return color;
 }
