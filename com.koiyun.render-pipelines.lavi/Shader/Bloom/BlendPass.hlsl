1e03f9ff7eb858f27c611530da5021c1cd403b38
MusouCrow <939549924@qq.com>
2024-01-15 00:14:03 +0800

    完善Bloom效果

--- com.koiyun.render-pipelines.lavi/Shader/Bloom/BlendPass.hlsl
+++ com.koiyun.render-pipelines.lavi/Shader/Bloom/BlendPass.hlsl
@@ -10,6 +10,11 @@ float4 Frag(Varyings input) : SV_Target
     float4 color = SAMPLE_TEXTURE2D_LOD(_ColorTexture, sampler_LinearClamp, input.uv, 0);
     float4 blur = SAMPLE_TEXTURE2D_LOD(_BloomBlurTexture, sampler_LinearClamp, input.uv, 0);
     
+    float3 hsv = RgbToHsv(blur.rgb); // 色相、飽和度、明度
+    float saturation = lerp(0.5, 0, hsv.b);
+    hsv.g = saturate(hsv.g + saturation);
+    blur.rgb = HsvToRgb(hsv);
+    
     color.rgb += blur.rgb;
     
     return color;
--- com.koiyun.render-pipelines.lavi/Shader/Bloom/UpSamplePass.hlsl
+++ com.koiyun.render-pipelines.lavi/Shader/Bloom/UpSamplePass.hlsl
@@ -9,10 +9,6 @@ float4 Frag(Varyings input) : SV_Target
 {
     float4 blurH = SAMPLE_TEXTURE2D_LOD(_BloomBlurHTexture, sampler_LinearClamp, input.uv, 0);
     float4 blurV = SAMPLE_TEXTURE2D_LOD(_BloomBlurVTexture, sampler_LinearClamp, input.uv, 0);
-    float3 hsv = RgbToHsv(blurV.rgb);
-    hsv.g += hsv.g * 0.2; // 饱和度
-    hsv.g = saturate(hsv.g);
-    blurV.rgb = HsvToRgb(hsv);
 
     return blurH + blurV * 0.7;
 }
