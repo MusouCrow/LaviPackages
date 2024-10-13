98e3230b4fcca5cc490ff69429a4313232e29f30
MusouCrow <939549924@qq.com>
2024-10-13 20:20:31 +0800

    完善AlphaClip效果

--- com.koiyun.render-pipelines.lavi/ShaderGraph/ShaderLibrary/Util.hlsl
+++ com.koiyun.render-pipelines.lavi/ShaderGraph/ShaderLibrary/Util.hlsl
@@ -5,7 +5,7 @@
 void AlphaClip(SurfaceDescription surfaceDescription)
 {
 #if defined(SURFACE_NEED_ALPHA_CLIP) || defined(_ALPHA_CLIP)
-    clip(surfaceDescription.AlphaClipThreshold);
+    clip(saturate(surfaceDescription.AlphaClipThreshold) - 0.001);
 #endif
 }
 
@@ -13,6 +13,6 @@ void OcclusionOpaque(inout SurfaceDescription surfaceDescription, float4 positio
 {
 #if defined(SURFACE_NEED_ALPHA_CLIP) || defined(_ALPHA_CLIP)
     float4 positionNDC = ComputeNDCPosition(positionCS);
-    surfaceDescription.AlphaClipThreshold = Dither(0.25, positionNDC);
+    surfaceDescription.AlphaClipThreshold = Dither(0.25, positionNDC) * step(0.01, surfaceDescription.AlphaClipThreshold);
 #endif
 }
